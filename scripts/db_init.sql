66e4504db2fe059a8ad7c0716ae12e73d27a942f
julian31186 <julian31186@gmail.com>
2023-01-11 17:01:16 -0500

    empty form validation // create user routes to -> create article and automatically logs in user

--- scripts/db_init.sql
+++ scripts/db_init.sql
@@ -19,3 +19,15 @@ create table User (
 
     primary key (`UserID`)
 );
+
+
+drop table if exists Subscribe;
+create table Subscribe (
+    SubscriptionID int auto_increment not null,       
+    SubscriberID int not null,
+    SubscribeeID int not null,
+
+    primary key (`SubscriptionID`),
+    foreign key (`SubscriberID`) references User(`UserID`),
+    foreign key (`SubscribeeID`) references User(`UserID`)
+);

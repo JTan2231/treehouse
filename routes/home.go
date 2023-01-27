eb3085540e317addbe1f58439dcb11305241d1d9
joey <j.tan2231@gmail.com>
2023-01-26 23:27:37 -0500

    formatting

--- routes/home.go
+++ routes/home.go
@@ -1,7 +1,7 @@
 package routes
 
 import (
-    "fmt"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"treehouse/config"
@@ -15,10 +15,10 @@ type HomeArticle struct {
 }
 
 func getSubscriptionArticles(userID int) ([]HomeArticle, error) {
-    dbConn := db.GetDB()
+	dbConn := db.GetDB()
 
-    rows, err := dbConn.Query(
-        `select
+	rows, err := dbConn.Query(
+		`select
             a.Title,
             a.Slug,
             u.Username
@@ -27,35 +27,35 @@ func getSubscriptionArticles(userID int) ([]HomeArticle, error) {
         inner join User u on u.UserID = s.SubscribeeID
         where a.UserID = s.SubscribeeID`, userID)
 
-    if err != nil {
-        return nil, err
-    }
+	if err != nil {
+		return nil, err
+	}
 
-    var articles []HomeArticle
+	var articles []HomeArticle
 
-    if rows != nil {
-        defer rows.Close()
-        for rows.Next() {
-            var article HomeArticle
+	if rows != nil {
+		defer rows.Close()
+		for rows.Next() {
+			var article HomeArticle
 
-            if err := rows.Scan(&article.Title, &article.Slug, &article.Username); err != nil {
-                return nil, err
-            }
+			if err := rows.Scan(&article.Title, &article.Slug, &article.Username); err != nil {
+				return nil, err
+			}
 
-            articles = append(articles, article)
-        }
-    }
+			articles = append(articles, article)
+		}
+	}
 
-    fmt.Println(articles);
+	fmt.Println(articles)
 
-    return articles, nil
+	return articles, nil
 }
 
 func getExploreArticles(userID int) ([]HomeArticle, error) {
-    dbConn := db.GetDB()
+	dbConn := db.GetDB()
 
-    rows, err := dbConn.Query(
-        `select distinct
+	rows, err := dbConn.Query(
+		`select distinct
             a.Title,
             a.Slug,
             u.Username
@@ -71,48 +71,47 @@ func getExploreArticles(userID int) ([]HomeArticle, error) {
         inner join User u on u.UserID = s.SubscribeeID
         where a.UserID = s.SubscribeeID`, userID, userID)
 
-    if err != nil {
-        return nil, err
-    }
+	if err != nil {
+		return nil, err
+	}
 
-    var articles []HomeArticle
+	var articles []HomeArticle
 
-    if rows != nil {
-        defer rows.Close()
-        for rows.Next() {
-            var article HomeArticle
+	if rows != nil {
+		defer rows.Close()
+		for rows.Next() {
+			var article HomeArticle
 
-            if err := rows.Scan(&article.Title, &article.Slug, &article.Username); err != nil {
-                return nil, err
-            }
+			if err := rows.Scan(&article.Title, &article.Slug, &article.Username); err != nil {
+				return nil, err
+			}
 
-            articles = append(articles, article)
-        }
-    }
+			articles = append(articles, article)
+		}
+	}
 
-    return articles, nil
+	return articles, nil
 }
-
 
 func ServeHome(c *gin.Context) {
 	session, _ := config.Store.Get(c.Request, "session")
 
 	username, ok := session.Values["username"]
 	if ok {
-        userID := session.Values["userID"].(int)
-        subscriptionArticles, err := getSubscriptionArticles(userID)
-        exploreArticles, err := getExploreArticles(userID)
+		userID := session.Values["userID"].(int)
+		subscriptionArticles, err := getSubscriptionArticles(userID)
+		exploreArticles, err := getExploreArticles(userID)
 
-        if err != nil {
-            c.IndentedJSON(400, gin.H{"errors": err})
-            return
-        }
+		if err != nil {
+			c.IndentedJSON(400, gin.H{"errors": err})
+			return
+		}
 
 		c.HTML(http.StatusOK, "home.tmpl", gin.H{
-			"signedInUsername": username,
-			"subscriptionArticles":         subscriptionArticles,
-            "exploreArticles":          exploreArticles,
-			"count":            len(subscriptionArticles),
+			"signedInUsername":     username,
+			"subscriptionArticles": subscriptionArticles,
+			"exploreArticles":      exploreArticles,
+			"count":                len(subscriptionArticles),
 		})
 	} else {
 		c.HTML(200, "404_redirect.tmpl", gin.H{

d4a939d130c2d70fd535bd39588c1992b28c2166
joey <j.tan2231@gmail.com>
2023-02-04 22:02:52 -0500

    menu dropdown

--- scripts/db_init.sql
+++ scripts/db_init.sql
@@ -2,9 +2,11 @@ drop table if exists Comment;
 drop table if exists Subscribe;
 drop table if exists Favorite;
 drop table if exists Article;
+drop table if exists Profile;
 drop table if exists User;
 
 \. user.sql
+\. profile.sql
 \. article.sql
 \. subscribe.sql
 \. comment.sql
